6d3765d96ed300d38d473d94c9026feb3af05ada
RANDRIANASOLOFranco <randrianasolofranco@gmail.com>
2020-05-08 19:43:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$('#delete').on('click', function(){
+    affichage.textContent = affichage.textContent + "delete";
+});
+$('#delete').on('click', function(){
+    affichage.textContent = affichage.textContent + "delete";
+});
+$('#restediv').on('click', function(){
+    affichage.textContent = affichage.textContent + "%";
+});
+$('#division').on('click', function(){
+    affichage.textContent = affichage.textContent + "/";
+});
+$('#sept').on('click', function(){
+    affichage.textContent = affichage.textContent + "7";
+});
+$('#huit').on('click', function(){
+    affichage.textContent = affichage.textContent + "8";
+});
+$('#neuf').on('click', function(){
+    affichage.textContent = affichage.textContent + "9";
+});
+$('#multiplication').on('click', function(){
+    affichage.textContent = affichage.textContent + "*";
+});
+$('#quatre').on('click', function(){
+    affichage.textContent = affichage.textContent + "4";
+});
+$('#cinq').on('click', function(){
+    affichage.textContent = affichage.textContent + "5";
+});
+$('#six').on('click', function(){
+    affichage.textContent = affichage.textContent + "6";
+});
+$('#soustraction').on('click', function(){
+    affichage.textContent = affichage.textContent + "-";
+});
+$('#un').on('click', function(){
+    affichage.textContent = affichage.textContent + "1";
+});
